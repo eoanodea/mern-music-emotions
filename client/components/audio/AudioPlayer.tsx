a17b96e6c156ff49d74f973b50ace087564f3a5a
Eoan O Dea <odeaeoan@gmail.com>
2020-04-24 12:44:57 +0100

    Successfully implemented audio and input functionality for audio player using react refs

--- client/components/audio/AudioPlayer.tsx
+++ client/components/audio/AudioPlayer.tsx
@@ -2,15 +2,16 @@
  * File: AudioPlayer.tsx
  * Project: mern-music-emotions
  * Version: 1.0.0
- * File Created: Wednesday, 22nd April 2020 6:58:33 pm
+ * File Created: Thursday, 23rd April 2020 9:36:20 pm
  * Author: Eoan O'Dea - eoan@wspace.ie
  * ---------------
- * File Description: Plays audio for a single track
- * Last Modified: Wednesday, 22nd April 2020 11:03:13 pm
+ * File Description:
+ * Last Modified: Friday, 24th April 2020 12:17:08 pm
  * Modified By: Eoan O'Dea - eoan@wspace.ie
  * ---------------
  * Copyright 2020 - WebSpace
  */
+
 
 import React, { Component } from "react";
 
@@ -20,82 +21,84 @@ type IProps = {
 
 class AudioPlayer extends Component<IProps> {
     audioRef: any;
-  
-//   currentTimeInterval: any;
-  // slider: React.RefObject<HTMLInputElement>;
-
-  // private slider: React.createRef<HTMLInputElement>()
-//   audioRef = React.useRef<HTMLAudioElement>(null);
-
+    currentTimeInterval: any;
+    sliderRef: any
   constructor(props: Readonly<IProps>) {
     super(props);
     
 
     this.audioRef = React.createRef<HTMLAudioElement>();
-    // this.currentTimeInterval = null;
-
+    this.sliderRef = React.createRef<HTMLInputElement>();
+    this.currentTimeInterval = null;
   }
 
-  // initializeAudioPlayer() {
-  //   // this.slider.value = 0;
-	// 	// this.currentTimeInterval = null;
-  //   const duration = this.audio!.duration || 0
-  //   const app = this
-	// 	// Get duration of the song and set it as max slider value
-	// 	this.audio!.onloadedmetadata = function() {
-	// 		app.setState({duration});
-	// 	}
+  componentDidMount() {
+    this.initializeAudioPlayer()
+  }
+
+  /**
+   * Controls and listeners for the audio and range input
+   */
+  initializeAudioPlayer = () => {
+    const duration = this.audioRef.duration || 0
+    const app = this
+		// Get duration of the song and set it as max slider value
+		this.audioRef.onloadedmetadata = function() {
+			app.setState({duration});
+		}
 		
-	// 	// Sync slider position with song current time
-	// 	this.audio!.onplay = () => {
-	// 		this.currentTimeInterval = setInterval( () => {
-	// 			this.slider!.value = this.audio!.currentTime;
-	// 		}, 500);
-	// 	};
+		// Sync slider position with song current time
+		this.audioRef.onplay = () => {
+      this.sliderRef.max = this.audioRef.duration;
+			this.currentTimeInterval = setInterval( () => {
+        this.sliderRef.value = this.audioRef.currentTime;
+			}, 500);
+		};
 		
-	// 	this.audio!.onpause = () => {
-	// 		clearInterval(this.currentTimeInterval);
-	// 	};
+		this.audioRef.onpause = () => {
+			clearInterval(this.currentTimeInterval);
+		};
 		
-	// 	// Seek functionality
-	// 	this.slider!.onchange = (e: React.ChangeEvent<HTMLInputElement>) => {
-	// 		clearInterval(this.currentTimeInterval);
-	// 		this.audio!.currentTime = parseInt(e.target.value);
-	// 	};
-  // }
+		// Seek functionality
+		this.sliderRef.onchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+			clearInterval(this.currentTimeInterval);
+			this.audioRef.currentTime = e.target.value;
+		};
+  }
 
   handleAudioPlayer = (time: any) => {
     console.log('time!', time, typeof time)
   }
 
     handlePlay = () => {
-    // this.audio!.play();
-        if(!this.audioRef) {
-            console.log('not here partner🦆')
-        } else {
-            console.log(this.audioRef.current, 'play!')
+        if(this.audioRef) {
+            this.audioRef.play()
         }
     }
 
-    handleStop() {
-        // this.audio!.currentTime = 0;
-        // this.slider!.value = 0;
-        // this.audio!.pause(); 
+    handleStop = () => {
+        if(this.audioRef && this.sliderRef) {
+          this.audioRef.currentTime = 0;
+          this.sliderRef.value = 0;
+          this.audioRef.pause(); 
+        }
     }
 
     render() {
         const { src } = this.props
 
-        console.log('src', src, this.audioRef)
         return (
             <div>
                 <audio ref={(input) => {this.audioRef = input}} 
-                //ref={this.audioRef} 
                 src={src} />
-            {/* <audio ref={(audio) => { this.audioRef = audio }} src={src} /> */}
+            
+            <input type="range" ref={(input) => {this.sliderRef = input}} min="0" defaultValue="0" />
             <input type="button" value="Play"
                 onClick={ this.handlePlay } />
+            <input type="button" value="Stop"
+                onClick={ this.handleStop } />
             </div>
+            
         );
     }
 }
