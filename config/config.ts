86fb2fa4fa152aaf52dca126e3b20c11dff0682a
Eoan O Dea <odeaeoan@gmail.com>
2020-04-24 19:37:41 +0100

    Updated ENV_MODE to NODE_ENV in config

--- config/config.ts
+++ config/config.ts
@@ -17,7 +17,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const config = {
-    env: process.env.ENV_MODE || "development",
+    env: process.env.NODE_ENV || "development",
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET, 
     mongoUri: process.env.MONGO_URI || "",
